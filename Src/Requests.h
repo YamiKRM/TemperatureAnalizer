15bc6720b1ecb61be84f5c0c52644bd4f414672b
YamiKRM <100734980+YamiKRM@users.noreply.github.com>
2023-06-02 22:59:23 -0500

    Update Requests.h
    - Added a parameter for the HTTP headers in functions get_request() and post_request().
    
    - Created a function called get_response_value() to read the value from a HTTP response and save into a buffer.
    
    -Added the URL related to the HTTP request to get the temperature from Ubidots.

--- Src/Requests.h
+++ Src/Requests.h
@@ -1,25 +1,30 @@
 #pragma once
-
 #include <vector>
+
+#include "HTTPRequest.hpp"
+#include "BuffReader.h"
 
 #ifndef _REQUESTS
 #define _REQUESTS
 #endif // !_RQEST
 
-
-#define GET "GET"
-#define POST "POST"
-
 #define API_KEY "BBFF-be5ac80954fb00ed9a19b784241fc9ef47f"
+
 #define API_TOKEN_REQUEST "http://industrial.api.ubidots.com/api/v1.6/auth/token"
+#define TEMPERATURE_REQUEST "http://industrial.api.ubidots.com/api/v1.6/devices/esp/temperature/lv"
+
 
 typedef std::vector<std::pair<std::string, std::string>> HEADERS;
+
+#pragma comment(lib, "ws2_32.lib") //Add WinSock2.h
 
 namespace requests
 {
 
-	void get_request(const char* url);
+	http::Response get_request(const char* url, HEADERS& headers);
 
-	void post_request(const char* url, HEADERS &headers);
+	http::Response post_request(const char* url, HEADERS &headers);
+
+	void get_response_value(const http::Response& response, BuffReader* reader);
 
 }
